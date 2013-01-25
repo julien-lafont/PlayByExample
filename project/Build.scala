8ae340199ec8f388c462e4e5e44104d9a5a3242f
Étienne Vallette d'Osia <evo@zenexity.com>
2013-01-25 11:36:18 +0100

    Use ReactiveMongo 0.8

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,11 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.8"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
   )
 
 }
