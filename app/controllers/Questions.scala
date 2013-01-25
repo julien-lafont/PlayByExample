c84bc30ee24719b592b9f1c5c8cba786ead07cc5
Pascal Voitot <pascal.voitot@mandubian.org>
2013-01-25 15:06:22 +0100

    question can be created in mongo

---
+++ app/controllers/Questions.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+import models._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
+
+object Questions extends Controller {
+
+  val createReads = (
+    (__ \ 'author).read[String] and
+    (__ \ 'title).read[String] and
+    (__ \ 'play).read[String] and
+    (__ \ 'lang).read[String]
+  ).tupled
+
+  def create = Action(parse.json){ request =>
+    
+    request.body.validate(createReads).map{ case (author, title, play, lang) =>
+      // TODO create gist
+
+      val gistId = ""
+      val tags = Seq(Tag(play), Tag(lang))
+      val q = Question(author, title, gistId, tags)
+
+      Async{
+        Question.insert(q).map { lasterror =>
+          Ok(Json.obj("status" -> "OK"))
+        }.recover{ case e => 
+          InternalServerError( Json.obj("status" -> "OK", "error" -> "mongo exception %s".format(e.getMessage) ) )
+        }
+      }
+    }.recoverTotal{ e =>
+      BadRequest(Json.obj("status" -> "KO", "error" -> JsError.toFlatJson(e)))
+    }
+  }
+}
--- app/models/Question.scala
+++ app/models/Question.scala
@@ -2,13 +2,57 @@ package models
 
 import org.joda.time._
 
+import scala.concurrent._
+
+import reactivemongo.api._
+import reactivemongo.bson._
+import reactivemongo.bson.handlers.DefaultBSONHandlers._
+import reactivemongo.core.commands.LastError
+
+import play.modules.reactivemongo._
+import play.modules.reactivemongo.PlayBsonImplicits._
+
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+
+import play.api.Play.current
+
 case class Question(
   authorID: GHID,
   title: String,
   gistID: GHGistID,
-  tags: Seq[Tag],
-  createdAt: DateTime,
-  updatedAt: DateTime
+  tags: Seq[Tag] = Seq(),
+  createdAt: DateTime = DateTime.now,
+  updatedAt: DateTime = DateTime.now
 )
 
 case class Tag(name: String) extends AnyVal
+
+object Tag extends Function1[String, Tag]{
+  implicit val tagReads = (__ \ "name").read[String].map( n => Tag(n) )
+  implicit val tagWrites = Writes{ t: Tag => JsString(t.name) }
+}
+
+object Question extends Function6[String, String, String, Seq[Tag], DateTime, DateTime, Question] {
+  val db = ReactiveMongoPlugin.db
+  lazy val coll = db("questions")
+
+  implicit val fmt = Json.format[Question]
+
+  val generateId = (__ \ '_id \ '$oid).json.put( JsString(BSONObjectID.generate.stringify) )
+  def dateToMongo(field: String) =  (__ \ field \ '$date).json.copyFrom( (__ \ field).json.pick )
+
+  val transformer = __.json.update( (generateId and dateToMongo("createdAt") and dateToMongo("updatedAt")).reduce )
+
+  def insert(q: Question)(implicit ex: ExecutionContext): Future[LastError] = {
+    val js = Json.toJson(q)(fmt)
+    println("js: "+ js.transform(transformer))
+    js.transform(transformer).map{ js =>
+      coll.insert(js)  
+    }.recoverTotal{ e =>
+      Future.failed(new RuntimeException(e.toString))
+    }
+    
+  }
+}
