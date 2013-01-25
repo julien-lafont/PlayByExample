679958037d88794adef919940eeb1a231d487dcb
Julien Lafont <jla@zenexity.com>
2013-01-25 17:17:57 +0100

    API Github: Récupérer la liste des réponses et la question

--- app/controllers/Api.scala
+++ app/controllers/Api.scala
@@ -7,19 +7,39 @@ import play.api.libs.concurrent.Execution.Implicits._
 
 import play.api.libs.json._
 import play.api.libs.json.Json._
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
 import concurrent.Future
 
 object Api extends GithubOAuthController {
 
-  def listGistsWithForks(gistId: Long) = Authenticated { implicit req =>
+  private def getLanguageField(name: String, field: String) = (__ \ name).json.copyFrom(
+    (__ \ "files").json.pick[JsObject].map{ js =>
+      js.fields.collectFirst{ case (k, v) if(v \ "language" != JsNull) => v \ field }.get
+    }
+  )
+
+  private def getQuestionField(name: String, field: String) = (__ \ name).json.copyFrom(
+    (__ \ "files").json.pick[JsObject].map{ js =>
+      js.fields.collectFirst{ case (k, v) if(v \ "language" == JsNull) => v \ field }.get
+    }
+  )
+
+  private val fullTransfForks = (
+    (__ \ "url").json.pickBranch and
+    (__ \ "id").json.pickBranch and
+      getQuestionField("questionUrl", "raw_url") and
+      getQuestionField("questionContent", "content") and
+      getLanguageField("answerUrl", "raw_url") and
+      getLanguageField("answerContent", "content")
+    ).reduce
+
+  def listForks(gistId: Long) = Authenticated { implicit req =>
     Async {
       GithubWS.Gist.forksId(gistId).flatMap{ list =>
         val listJsonFuture = list.map(forkId =>
           GithubWS.Gist.get(forkId).map(json =>
-            // TODO EXTRAIRE DONNEES JSON
-            json
-          )
-        )
+            json.transform(fullTransfForks).getOrElse(JsNull)))
         Future.sequence(listJsonFuture).map(listJson =>
           Ok(Json.toJson(listJson))
         )
@@ -27,18 +47,15 @@ object Api extends GithubOAuthController {
     }
   }
 
-  /*def gistInfo(gistId: String) = Authenticated { implicit req =>
+  def getQuestion(gistId: Long) = Authenticated { implicit req =>
     Async {
-      /*val extractGist: Reads[JsObject] = (
-        (__ \ 'comments_url).json.pick and
-        (__ \ 'git_push_url).json.pick
-      ).reduce*/
-
-      GithubWS.Gist.get(4634703).map{ json =>
-        //val r = json.validate[extractGist]
-
-        Ok(json)
-      }
+      GithubWS.Gist.get(gistId).map(json =>
+        Ok(json.transform(
+          ( getQuestionField("questionUrl", "raw_url") and
+            getQuestionField("questionContent", "content")
+          ).reduce).getOrElse(JsNull))
+      )
     }
-  }*/
+  }
+
 }
